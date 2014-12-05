60a25d3c5406e41ba7be0b80d1dd5e975639b8b9
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-04 21:04:47 -0800

    git ignores yelp api keys, adds helpers.js for sending response from server, and almost completes post to /imhungry needs to filter results to fit user specified ratings

---
+++ server/helpers.js
@@ -0,0 +1,18 @@
+var exports = exports.module = {};
+
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10
+};
+
+
+var sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  headers['Content-Type'] = 'application/json';
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(data));
+};
+
+exports.sendResponse = sendResponse;
