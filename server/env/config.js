8d51eab16b38cae7196738964014c31993f679f2
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-06 16:15:59 -0800

    fixes package.json and modules, localhost is running!

--- server/env/config.js
+++ server/env/config.js
@@ -1,8 +1,8 @@
 var exports = module.exports = {};
 
 exports.keys = {
-  consumer_key: "enter consumer key", 
-  consumer_secret: "enter consumer secret",
-  token: "enter token",
-  token_secret: "enter token secret"
+  consumer_key: "RTmlWWKM1e0yJ03HBRNcrA", 
+  consumer_secret: "Li3ZTBFFqygbBKfwdedQR5MCYFQ",
+  token: "lUakp9DqE6NVwNKNtqybo-qt6-ymqfwY",
+  token_secret: "UgCZb8K5vHzaHMTvhr8_uNNG7rs"
 };
