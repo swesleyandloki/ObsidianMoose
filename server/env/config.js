b141527df773512b9ee92c4d6c54f219a66f7953
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-08 12:05:39 -0800

    solves merge conflicts

--- server/env/config.js
+++ server/env/config.js
@@ -1,9 +1,8 @@
 var exports = module.exports = {};
 
 exports.keys = {
-  consumer_key: "RTmlWWKM1e0yJ03HBRNcrA", 
-  consumer_secret: "Li3ZTBFFqygbBKfwdedQR5MCYFQ",
-  token: "lUakp9DqE6NVwNKNtqybo-qt6-ymqfwY",
-  token_secret: "UgCZb8K5vHzaHMTvhr8_uNNG7rs"
+  consumer_key: "xxxxxx", 
+  consumer_secret: "xxxxx",
+  token: "xxxxx",
+  token_secret: "xxxxxx"
 };
-
