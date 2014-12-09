32b4fb90317d927c0b1cf5a2de092ff483df38e0
Catherine Bui <contactcatherinebui@gmail.com>
2014-12-08 16:14:57 -0800

    adding api keys

--- server/env/config.js
+++ server/env/config.js
@@ -1,9 +1,9 @@
 var exports = module.exports = {};
 
 exports.keys = {
-  consumer_key: "", 
-  consumer_secret: "",
-  token: "",
-  token_secret: ""
+consumer_key: "RTmlWWKM1e0yJ03HBRNcrA", 
+ consumer_secret: "Li3ZTBFFqygbBKfwdedQR5MCYFQ",
+ token: "lUakp9DqE6NVwNKNtqybo-qt6-ymqfwY",
+ token_secret: "UgCZb8K5vHzaHMTvhr8_uNNG7rs"
 };
 
